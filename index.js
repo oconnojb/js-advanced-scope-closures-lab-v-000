4cba1c2ef6192369ddd3643c0f0cd3045657ef7a
John O'Connor <oconnojb@gmail.com>
2019-03-03 21:48:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,3 +18,14 @@ function produceTipCalculator(perc) {
     return bill * perc;
   }
 }
+
+function createDriver() {
+  let DriverId = 0;
+  // return the class
+  return class {
+    constructor(name) {
+      this.name = name;
+      this.id = ++DriverId;
+    }
+  }
+}
