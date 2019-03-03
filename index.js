dc3ff0f251e38bb2fe813cb1d311609ac6f92c10
John O'Connor <oconnojb@gmail.com>
2019-03-03 21:35:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ function produceDrivingRange(range) {
     let b1 = block1.slice(0, -2);
     let b2 = block2.slice(0, -2);
     if (b1-b2 < range) {
-      console.log(range-(b1-b2))
+      console.log("range: " + range);
+      console.log("b1: " + b1 + " b2: " + b2)
+      console.log(range-(b1-b2));
     }
   }
 }
