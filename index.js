040140dce2e5abbaac7f3adf0f6533b057aac8e8
John O'Connor <oconnojb@gmail.com>
2019-03-03 21:48:51 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function produceDrivingRange(range) {
+  return function(block1, block2) {
+    let b1 = block1.slice(0, -2);
+    let b2 = block2.slice(0, -2);
+    let dif = Math.abs(b1 - b2);
+
+    if (dif > range) {
+      return (dif - range) + " blocks out of range";
+    }
+    else {
+      return "within range by " + (range - dif);
+    }
+  }
+}
+
+function produceTipCalculator(perc) {
+  return function(bill) {
+    return bill * perc;
+  }
+}
+
+function createDriver() {
+  let DriverId = 0;
+  // return the class
+  return class {
+    constructor(name) {
+      this.name = name;
+      this.id = ++DriverId;
+    }
+  }
+}
